7af9e7db84f42387d0ea33c698fa1205c66df39b
Dhanush <dhanushraju24@gmail.com>
2026-06-27 16:34:42 +0530

    Completed CommBank Forage project

--- CommBank-Server/Models/Goal.cs
+++ CommBank-Server/Models/Goal.cs
@@ -25,6 +25,9 @@ public class Goal
     [BsonRepresentation(BsonType.ObjectId)]
     public List<string>? TagIds { get; set; }
 
+   [BsonElement("icon")]
+    public string? Icon { get; set; }
+
     [BsonRepresentation(BsonType.ObjectId)]
     public string? UserId { get; set; }
 }
